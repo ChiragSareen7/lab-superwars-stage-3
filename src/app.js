21cf9ffda1a9abcdea7320d8423e7d2faeebe7e0
ChiragSareen7 <chiragsareen8054@gmail.com>
2024-02-12 16:06:57 +0530

    done

--- src/app.js
+++ src/app.js
@@ -23,36 +23,55 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    let detailedPlayers = [];
+    // Create players using for loop
+    // Type your code here
+    for(let i=0;i<players.length;i++){
+        detailedPlayers[i]={
+            strength: getRandomStrength(),
+            image: `./images/super-${i+1}.png`,
+            name:`${players[i]}`,
+            type: i % 2 === 0 ? "hero" : "villain"
+        }
 
-    // Instead of forloop use Map method
-    // Code here
+    }
 
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
+    // Return a random integer (0,100]
+    // Note: You can use Math.random() and Math.ceil()
+    let unpredictable=Math.ceil(Math.random() * 100);
+    return unpredictable;
 }
 
-// Build player template
 const buildPlayers = (players, type) => {
     let fragment = '';
+    for(let i=0;i<players.length;i++){
+        if(players[i].type===type){
+            fragment+=`<div class="player">
+            <img src="${players[i].image}" alt="">
+            <div class="name">${players[i].name}</div>
+            <div class="strength">${players[i].strength}</div>
+         </div>`;
+        }
+    }
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
+    // Loop through players and accumulate HTML template
+    // depending of type of player(hero|villain)
     // Type your code here
 
     return fragment;
 }
-
 // Display players in HTML
 const viewPlayers = (players) => {
+
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
 
+}
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
